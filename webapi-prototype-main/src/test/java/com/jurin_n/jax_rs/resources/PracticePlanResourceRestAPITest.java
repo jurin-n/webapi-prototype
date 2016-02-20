fceffe17d46865a65d8fb3285b29c2d164709fea
jurin-n <jurin.nakano@gmail.com>
2016-02-21 00:36:17 +0900

    カテゴリ化テスト対応。

---
+++ webapi-prototype-main/src/test/java/com/jurin_n/jax_rs/resources/PracticePlanResourceRestAPITest.java
@@ -0,0 +1,16 @@
+package com.jurin_n.jax_rs.resources;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(RestAPITests.class)
+public class PracticePlanResourceRestAPITest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ webapi-prototype-main/src/test/java/com/jurin_n/jax_rs/resources/RestAPITests.java
@@ -0,0 +1,6 @@
+package com.jurin_n.jax_rs.resources;
+
+//JUnitカテゴリ化テスト用のマーカークラス
+public interface RestAPITests {
+
+}

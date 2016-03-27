5c06bed0afdc5ea5db3f1178e4515637a9e04325
jurin-n <jurin.nakano@gmail.com>
2016-03-27 15:04:01 +0900

    JPAでSQL実行する機能を追加。

--- webapi-prototype-main/src/test/java/com/jurin_n/junit/dbutils/JPADataSetUtil.java
+++ webapi-prototype-main/src/test/java/com/jurin_n/junit/dbutils/JPADataSetUtil.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -18,45 +21,86 @@ public class JPADataSetUtil {
 	List<String> initSQLs = new ArrayList<>();
 	List<Table> tables = new ArrayList<>();
 
-	void setUpDataSetXML() throws SAXException, IOException, ParserConfigurationException {
-		InputStream is = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/jpa-dataset.xml");
+	void setUpDataSetXML(String xmlPath) throws SAXException, IOException, ParserConfigurationException {
+		InputStream is = new FileInputStream(xmlPath);
 		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
 		Element dataset = doc.getDocumentElement();
 
-		Element init = findChildByTag(dataset,"init");
-		initSQLs.add(init.getTextContent().trim());
-		
-		Element setup = findChildByTag(dataset,"setup");
-		List<Element> tables = findChildrenByTag(setup,"table");
-		for(Element table :tables){
-			Element columns = findChildByTag(table,"columns");
-			Element data = findChildByTag(table,"data");
-			
-			//TODO table名、カラム、データを格納する方法を検討。
-			Table tableData = new Table(
-					 table.getAttribute("name")
-					,columns.getTextContent().trim()
-					,data.getTextContent().trim()
-					);
-			System.out.println("tableData.getName() = " + tableData.getName());
-			System.out.println("tableData.getColumns().toString()= " + tableData.getColumns().toString());
-			System.out.println("tableData.getRows().toString()= " + tableData.getRows().toString());
+		Element init = findChildByTag(dataset, "init");
+		StringTokenizer initText = new StringTokenizer(init.getTextContent(), "\n");
+		while (initText.hasMoreTokens()) {
+			String initSQL = initText.nextToken().trim();
+			if (initSQL == null || initSQL.equals("")) {
+				continue;
+			}
+			initSQLs.add(initSQL);
+		}
+
+		Element setup = findChildByTag(dataset, "setup");
+		List<Element> tables = findChildrenByTag(setup, "table");
+		for (Element table : tables) {
+			Element columns = findChildByTag(table, "columns");
+			Element data = findChildByTag(table, "data");
+
+			// TODO table名、カラム、データを格納する方法を検討。
+			Table tableData = new Table(table.getAttribute("name"), columns.getTextContent().trim(),
+					data.getTextContent().trim());
+			this.tables.add(tableData);
 		}
 	}
-	
-	//TODO JPAのnative Queryでデータセットする処理追加
-	
+
+	String createInsertSQL(Table table) {
+		String sql = "insert into " + table.getName();
+		String columns = "";
+		String bindParams = "";
+
+		for (int i = 0; i < table.getColumns().size(); i++) {
+			if (i == 0) {
+				columns += table.getColumns().get(i);
+				bindParams += "?";
+			} else {
+				columns = columns + "," + table.getColumns().get(i);
+				bindParams += ",?";
+			}
+		}
+		return sql + "(" + columns + ") values(" + bindParams + ")";
+	}
+
+	void executeSQL(EntityManager em) {
+		em.getTransaction().begin();
+
+		// init
+		for (String initSQL : initSQLs) {
+			System.out.println("initSQL = " + initSQL);
+			em.createNativeQuery(initSQL).executeUpdate();
+		}
+
+		// setup
+		for (Table table : tables) {
+			Query q = em.createNativeQuery(createInsertSQL(table));
+			// 行単位の処理
+			for (List<String> row : table.getRows()) {
+				// 列単位の処理
+				for (int i = 0; i < table.getColumns().size(); i++) {
+					q.setParameter(i + 1, row.get(i));
+				}
+				q.executeUpdate();
+			}
+		}
+		em.getTransaction().commit();
+	}
+
 	static Element findChildByTag(Element self, String name) {
-		return findChildrenByTag(self,name).get(0);
+		return findChildrenByTag(self, name).get(0);
 	}
 
 	static List<Element> findChildrenByTag(Element self, String name) {
 		List<Element> list = new ArrayList<>();
 		NodeList children = self.getChildNodes();
 		for (int i = 0; i < children.getLength(); i++) {
-			if(children.item(i) instanceof Element){
-				Element e = (Element)children.item(i);
-				if(e.getTagName().equals(name)){
+			if (children.item(i) instanceof Element) {
+				Element e = (Element) children.item(i);
+				if (e.getTagName().equals(name)) {
 					list.add(e);
 				}
 			}
--- webapi-prototype-main/src/test/java/com/jurin_n/junit/dbutils/JPADataSetUtilTest.java
+++ webapi-prototype-main/src/test/java/com/jurin_n/junit/dbutils/JPADataSetUtilTest.java
@@ -6,15 +6,24 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import com.jurin_n.junit.rules.JPAResource;
+
 public class JPADataSetUtilTest {
+	@Rule
+	public JPAResource jpa = new JPAResource();
 
+	/*
+	 * テスト実行前に下記スクリプトでテーブル作成 CREATE TABLE t_jpautil_test (column1 VARCHAR(255),
+	 * column2 VARCHAR(255),column3 Integer)
+	 */
 	@Test
-	public void test() throws SAXException, IOException, ParserConfigurationException {
-		JPADataSetUtil sut  = new JPADataSetUtil();
-		sut.setUpDataSetXML();
+	public void SQL実行テスト() throws SAXException, IOException, ParserConfigurationException {
+		JPADataSetUtil sut = new JPADataSetUtil();
+		sut.setUpDataSetXML(System.getProperty("user.dir") + "/src/test/resources/jpa-dataset.xml");
+		sut.executeSQL(jpa.getEm());
 	}
-
 }

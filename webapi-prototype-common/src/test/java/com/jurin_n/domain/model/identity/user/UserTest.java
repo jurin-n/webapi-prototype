b75252459831db130638093f743bd6326e89a460
jurin-n <jurin.nakano@gmail.com>
2016-04-03 14:54:04 +0900

    整形

--- webapi-prototype-common/src/test/java/com/jurin_n/domain/model/identity/user/UserTest.java
+++ webapi-prototype-common/src/test/java/com/jurin_n/domain/model/identity/user/UserTest.java
@@ -1,4 +1,5 @@
 package com.jurin_n.domain.model.identity.user;
+
 import static org.junit.Assert.*;
 
 import java.util.HashSet;
@@ -14,70 +15,58 @@ import com.jurin_n.domain.model.identity.role.RoleValue;
 
 public class UserTest {
 
-	private String A_USER_ID_FOR_TEST = "user001";
-	private String A_USER_NAME_FOR_TEST = "テスト　太郎";
-	private String A_ROLE_ID_FOR_TEST = "role001";
-	private Set<Role> roles;
-	private Role role;
-	private Set<PermissionValue> permissions;
+    private String A_USER_ID_FOR_TEST = "user001";
+    private String A_USER_NAME_FOR_TEST = "テスト　太郎";
+    private String A_ROLE_ID_FOR_TEST = "role001";
+    private Set<Role> roles;
+    private Role role;
+    private Set<PermissionValue> permissions;
 
-	@Before
-	public void createRole(){
-		roles = new HashSet<>();
-		permissions = new HashSet<>();
+    @Before
+    public void createRole() {
+        roles = new HashSet<>();
+        permissions = new HashSet<>();
 
-		//writePlanのPermissionを保有するADMINロール作成
-		role = new Role();
-		role.setId(A_ROLE_ID_FOR_TEST);
-		role.setValue(RoleValue.ADMIN);
-		permissions.add(PermissionValue.writePlan);
-		permissions.add(PermissionValue.readPlan);
-		role.setPermissions(permissions);
+        // writePlanのPermissionを保有するADMINロール作成
+        role = new Role();
+        role.setId(A_ROLE_ID_FOR_TEST);
+        role.setValue(RoleValue.ADMIN);
+        permissions.add(PermissionValue.writePlan);
+        permissions.add(PermissionValue.readPlan);
+        role.setPermissions(permissions);
 
-		roles.add(role);
-	}
-	
-	@Test
-	public void inRoleメソッドの引数に渡したRoleがUserに含まれてる場合_inRoleメソッドはtrueを返す() {
-		User user = new User(
-				 new UserId(A_USER_ID_FOR_TEST)
-				,A_USER_NAME_FOR_TEST
-				,roles
-				,Status.ACTIVE);
+        roles.add(role);
+    }
 
-		assertTrue(user.inRole(RoleValue.ADMIN));
-	}
-	
-	@Test
-	public void inRoleメソッドの引数に渡したRoleがUserに含まれていない場合_inRoleメソッドはfalseを返す() {
-		User user = new User(
-				 new UserId(A_USER_ID_FOR_TEST)
-				,A_USER_NAME_FOR_TEST
-				,roles
-				,Status.ACTIVE);
+    @Test
+    public void inRoleメソッドの引数に渡したRoleがUserに含まれてる場合_inRoleメソッドはtrueを返す() {
+        User user = new User(new UserId(A_USER_ID_FOR_TEST),
+                A_USER_NAME_FOR_TEST, roles, Status.ACTIVE);
 
-		assertFalse(user.inRole(RoleValue.MEMBER));
-	}
+        assertTrue(user.inRole(RoleValue.ADMIN));
+    }
 
-	@Test
-	public void inPermissionメソッドの引数に渡したPermissionがUserに含まれている場合_inPermissionメソッドはtrueを返す() {
-		User user = new User(
-				 new UserId(A_USER_ID_FOR_TEST)
-				,A_USER_NAME_FOR_TEST
-				,roles
-				,Status.ACTIVE);
+    @Test
+    public void inRoleメソッドの引数に渡したRoleがUserに含まれていない場合_inRoleメソッドはfalseを返す() {
+        User user = new User(new UserId(A_USER_ID_FOR_TEST),
+                A_USER_NAME_FOR_TEST, roles, Status.ACTIVE);
 
-		assertTrue(user.inPermission(PermissionValue.writePlan));
-	}
-	
-	@Test
-	public void inPermissionメソッドの引数に渡したPermissionがUserに含まれていない場合_inPermissionメソッドはfalseを返す() {
-		User user = new User(
-				 new UserId(A_USER_ID_FOR_TEST)
-				,A_USER_NAME_FOR_TEST
-				,roles
-				,Status.ACTIVE);
+        assertFalse(user.inRole(RoleValue.MEMBER));
+    }
 
-		assertFalse(user.inPermission(PermissionValue.writeMenu));
-	}
+    @Test
+    public void inPermissionメソッドの引数に渡したPermissionがUserに含まれている場合_inPermissionメソッドはtrueを返す() {
+        User user = new User(new UserId(A_USER_ID_FOR_TEST),
+                A_USER_NAME_FOR_TEST, roles, Status.ACTIVE);
+
+        assertTrue(user.inPermission(PermissionValue.writePlan));
+    }
+
+    @Test
+    public void inPermissionメソッドの引数に渡したPermissionがUserに含まれていない場合_inPermissionメソッドはfalseを返す() {
+        User user = new User(new UserId(A_USER_ID_FOR_TEST),
+                A_USER_NAME_FOR_TEST, roles, Status.ACTIVE);
+
+        assertFalse(user.inPermission(PermissionValue.writeMenu));
+    }
 }
